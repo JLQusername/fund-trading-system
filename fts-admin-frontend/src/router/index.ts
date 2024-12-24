fb7e90649b0099d792c1468010f8626dfd611347
B907 <1841129348@qq.com>
2024-12-24 11:19:30 +0800

    feat：前端框架继续搭建

---
+++ fts-admin-frontend/src/router/index.ts
@@ -0,0 +1,12 @@
+//导入vue-router
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+//定义路由关系
+const routes:RouteRecordRaw[] = [
+
+]
+//创建路由器
+const router = createRouter({
+history: createWebHistory(),
+routes: routes
+});
+export default router
